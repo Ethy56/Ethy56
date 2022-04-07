3b4e13cea38a90db9ad31a7c1e89e07017efcdff
Ethy56 <Ethy56@users.noreply.github.com>
2022-04-06 22:33:01 -0400

    fixed missing key

--- components/channelsfor.js
+++ components/channelsfor.js
@@ -31,7 +31,7 @@ const ChannelsFor = props => {
         <div className="select-none px-4 py-2 flex flex-col justify-center gap-2">
             {items.map(item=>{
                 return (
-                    <Link href={"/?id="+ id + "&item="+item}>
+                    <Link key={id+item} href={"/?id="+ id + "&item="+item}>
                         <div className="cursor-pointer w-full h-[2.5vh] py-2 flex justify-left items-center transition-all text-gray-300 hover:text-gray-100 hover:bg-[#393c43]">
                             <Image src="https://cdn.discordapp.com/attachments/814236503000744016/961361927043444766/hashtagthing.png" width={20} height={20} />
                             <h1 className="text-md">{item}</h1>
